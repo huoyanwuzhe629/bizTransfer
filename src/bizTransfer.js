c8b90a477c10c3c9872d08214d71122547106221
xiongsheng <huoyanwuzhe629@163.com>
2016-10-25 18:57:56 +0800

    起步

--- src/bizTransfer.js
+++ src/bizTransfer.js
@@ -2,41 +2,156 @@
 * @Author: xiongsheng
 * @Date:   2016-10-24 15:10:54
 * @Last Modified by:   xiongsheng
-* @Last Modified time: 2016-10-24 17:10:05
+* @Last Modified time: 2016-10-25 18:52:12
 */
 
 'use strict';
 
+const html = `<div class="biz-transfer">
+    <div class="biz-transfer-list js-left-list">
+        <div class="biz-transfer-list-header">
+            <span>请选择人群</span>
+        </div>
+        <div class="biz-transfer-list-body">
+            <ul class="biz-transfer-list-content">
+            </ul>
+        </div>
+    </div>
+    <div class="biz-transfer-operation">
+        <button ><span>添加</span></button>
+        <button ><span>移除</span></button>
+    </div>
+    <div class="biz-transfer-list js-right-list">
+        <div class="biz-transfer-list-header">
+            <span>已选择人群</span>
+        </div>
+        <div class="biz-transfer-list-body">
+            <ul class="biz-transfer-list-content">
+            </ul>
+        </div>
+    </div>
+</div>`;
+const leftSelectPrefix = 'biz-transfer-left-list',
+    rightSelectPrefix = 'biz-transfer-right-list';
+
 export default class BizTransfer {
     constructor(transfer, options) {
         this.options = $.extend({}, options || {});
+        this.dataSource = this.options.dataSource || [];
         this.$el = $(transfer);
+        this.$el.append(html);
+
+        this.$leftListHeader = this.$el.find('.js-left-list .biz-transfer-list-header');
+        this.$rightListHeader = this.$el.find('.js-right-list .biz-transfer-list-header');
+        this.$leftListBody = this.$el.find('.js-left-list .biz-transfer-list-content');
+        this.$rightListBody = this.$el.find('.js-right-list .biz-transfer-list-content');
         this.init();
     }
 
     init() {
-        const html = `<div class="selectbox">
-            <div class="select-bar">
-                <select multiple="multiple" id="select1">
-                    <option value="超级管理员">超级管理员</option>
-                    <option value="信息发布员">信息发布员</option>
-                    <option value="财务管理员">财务管理员</option>
-                </select>
-            </div>
+        this.render();
+        this.createSelect();
+    }
 
-            <div class="btn-bar">
-                <p><span id="add"><input type="button" class="btn" value=">" title="移动选择项到右侧"></span></p>
-                <p><span id="add_all"><input type="button" class="btn" value=">>" title="全部移到右侧"></span></p>
-                <p><span id="remove"><input type="button" class="btn" value="<" title="移动选择项到左侧"></span></p>
-                <p><span id="remove_all"><input type="button" class="btn" value="<<" title="全部移到左侧"></span></p>
-            </div>
-            <div class="select-bar">
-                <select multiple="multiple" id="select2"><option value="系统管理员">系统管理员</option><option value="普通管理员">普通管理员</option></select>
-            </div>
-        </div>`;
+    render() {
+        if (this.dataSource.length) {
+            this.dataSource.map((value, index)=>{
+                this.$leftListBody.append(`<li class="biz-transfer-list-content-item"><span>${value.title}</span></li>`);
+            });
+        }
+        if (this.getTargets().length) {
+            this.getTargets().map((value, index)=>{
+                this.$rightListBody.append(`<li class="biz-transfer-list-content-item"><span>${value.title}</span></li>`);
+            })
+        }
+    }
 
-        this.$el.append(html);
+    //获得被选中项的id列表
+    getTargets() {
+        return this.dataSource.filter(data=>{
+            return data.chosen;
+        })
+    }
 
+    getTargetKeys() {
+        return Array.from(this.getTargets.keys());
+    }
+
+    /**
+     * 创建Checkbox控件
+     * @protected
+     */
+    createSelect() {
+        if (this.dataSource.length) {
+            this.$leftListHeader.prepend(`<input type="checkbox" title=" " class="js-leftSelectAll"/>`);
+            this.$rightListHeader.prepend(`<input type="checkbox" title=" " class="js-rightSelectAll"/>`)
+            this.dataSource.map((value, index)=>{
+                $(this.$leftListBody.find('li')[index]).prepend(`<input type="checkbox" title=""  key=${value.id} chosen=${value.chosen} /> `)
+            });
+            this.getTargets().map((value, index)=>{
+                $(this.$rightListBody.find('li')[index]).prepend(`<input type="checkbox" title=""  key=${value.id}/>`)
+            });
+        }
+        this.$el.find(':checkbox').bizCheckbox();
+        this.$leftListBody.find(':checkbox[chosen=true]').bizCheckbox('disable');
+    }
+
+    /**
+     * 创建Checkbox控件
+     * @protected
+     */
+    bindSelect() {
+        var self = this;
+        this.$main.on('click.bizTableSelectAll', '.biz-table-head th .biz-label', function(e) {
+            var selected = $(e.target).hasClass('biz-checkbox-checked'),
+                checkbox = self.$tableBody.find(':checkbox'),
+                tr = self.$tableBody.find('tr[class!="sum"]');
+            if (selected) {
+                checkbox.bizCheckbox('check');
+                tr.addClass('selected');
+                if (self.options.onSelect) {
+                    self.options.onSelect.call(self, self.options.data, e);
+                }
+            } else {
+                checkbox.bizCheckbox('uncheck');
+                tr.removeClass('selected');
+                if (self.options.onSelect) {
+                    self.options.onSelect.call(self, [], e);
+                }
+            }
+        }).on('click.bizTableSelectOne', '.biz-table-body td .biz-label', function(e) {
+            var selected = $(e.target).hasClass('biz-checkbox-checked'),
+                selectedCount = self.$tableBody.find('.biz-checkbox-checked').length,
+                selectAll = self.$tableHead.find(':checkbox'),
+                tr = $(e.target).parent().parent(),
+                rowCount = self.options.data.length;
+            if (selectedCount === rowCount) {
+                selectAll.bizCheckbox('check');
+            } else {
+                selectAll.bizCheckbox('uncheck');
+            }
+            if (selected) {
+                tr.addClass('selected');
+            } else {
+                tr.removeClass('selected');
+            }
+            if (self.options.onSelect) {
+                self.options.onSelect.call(self, $.map(self.getSelectedIndex(), function(item, index) {
+                    return self.options.data[item];
+                }), e);
+            }
+        });
+    }
+
+    /**
+     * 获取勾选行序号
+     * @return {Array}
+     * @protected
+     */
+    getSelectedIndex() {
+        return $.map(this.$tableBody.find('.biz-checkbox-checked'), function(label, index) {
+            return parseInt($(label).attr('for').replace(selectPrefix, ''), 10) - 1;
+        });
     }
 
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: xiongsheng
  * @Date:   2016-08-16 17:23:26
  * @Last Modified by:   xiongsheng
- * @Last Modified time: 2016-10-24 17:04:38
+ * @Last Modified time: 2016-10-25 18:06:37
  */
 
 'use strict';
@@ -10,4 +10,17 @@ import './bizTransfer';
 import './jquery.bizui.css';
 import './bizTransfer.css';
 
-$('#bizComponent').bizTransfer();
+const data = [
+    {
+        id: 1,
+        title: 'xiongsheng',
+        chosen: false
+    },{
+        id: 2,
+        title: 'guoqi',
+        chosen: true
+    },
+];
+$('#bizComponent').bizTransfer({
+    dataSource: data
+});
